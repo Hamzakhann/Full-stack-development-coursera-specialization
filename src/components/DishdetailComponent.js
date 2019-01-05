f14e16e8cfce8adb257e2b728a333e5577507c7b
Hamzakhann <hamzakhann66@gmail.com>
2019-01-05 10:45:48 +0500

    make menu component a dump component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,20 @@ import { Card, CardImg, CardText, CardBody,
 
 
 class DishDetail  extends Component {
+  constructor(props){
+    super(props)
+    console.log('oye ' ,this.props.dish)
+
+  }
+
+  componentDidMount(){
+    console.log('component did mount chal gya')
+  }
+  
+  componentDidMount(){
+    console.log('component did update chal gya')
+  }
+
   renderDish(dish){
     if(dish !== null){
       return(
@@ -29,7 +43,7 @@ class DishDetail  extends Component {
             <ul key={comment.id}  className="list-unstyled" >
               <li >{comment.comment}</li>
               <br/>
-              <li  >--{comment.author} , {comment.date} </li>
+              <li  >--{comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
             </ul>
           )
         })
@@ -40,15 +54,16 @@ class DishDetail  extends Component {
   }
 
   render(){
-  
     return(
+      <div className = "container" >
       <div className="row" >
       <div className="col-12 col-md-5 m-1" >
-      {this.renderDish(this.props.dish)}
+      {this.props.dish && this.renderDish(this.props.dish)}
       </div>
       <div className="col-12 col-md-5 m-1" >
       <h4>Comments</h4>
-      {this.props.dish !==null ?  this.renderComments(this.props.dish.comments): <div></div>}
+      {this.props.dish && this.renderComments(this.props.dish.comments)}
+      </div>
       </div>
       </div>
     )
