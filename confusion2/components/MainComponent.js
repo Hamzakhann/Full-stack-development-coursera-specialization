c07c144b79403463c5e682088247823c5dfaee16
Hamzakhann <hamzakhann66@gmail.com>
2019-02-03 22:15:09 +0500

    added side drawer

--- confusion2/components/MainComponent.js
+++ confusion2/components/MainComponent.js
@@ -1,17 +1,77 @@
 import React , {Component} from'react';
+import {View , Platform } from 'react-native';
 import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
+import Home from './HomeComponent';
+import Dishdetail from './DishdetailComponent';
+import {createStackNavigator , createAppContainer ,createDrawerNavigator } from 'react-navigation';
+
+
+const MenuNavigator = createStackNavigator({
+    Menu: {screen:Menu},
+    Dishdetail : {screen:Dishdetail}
+},{
+    initialRouteName:'Menu',
+    defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      },
+});
+
+
+const HomeNavigator = createStackNavigator({
+    Home: {screen:Home},
+},{
+    defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        headerTintColor: 'yellow',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          marginLeft:'40%'
+        },
+      },
+});
+
+
+const MainNavigator = createDrawerNavigator({
+    Home:{
+        screen:HomeNavigator,
+        defaultNavigationOptions:{
+            title:'Home',
+            drawerLabel:'Home'
+        }
+    },
+    Menu:{
+        screen:MenuNavigator,
+        defaultNavigationOptions:{
+            title:'Menu',
+            drawerLabel:'Menu'
+        }      
+    }
+},{
+    drawerBackgroundColor:'#D1C4E9'
+});
+
+
+
+const RootStack = createAppContainer(MainNavigator)
 
 class Main extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            dishes:DISHES
-        }
-    }
+  
     render(){
         return(
-            <Menu dishes={this.state.dishes} />
+
+        <View style={{flex:1 , paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}} >
+        {/* <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+        <RootStack/> 
+        </View>
         )
     }
 }
